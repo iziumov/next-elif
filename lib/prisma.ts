c7f2171ad4ca16fc359c1609d31c086dc3142345
iziumov <ihoriziumov@gmail.com>
2024-05-22 20:31:50 +0300

    * vercel prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-  var prisma: PrismaClient | undefined;
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
 
-export const prisma = globalThis.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+export { prisma };
